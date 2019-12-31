e443198ee3cea1fa349549595220c5f80390e728
yangkai <yangkai@localhost.localdomain>
2019-12-31 07:56:33 +0000

    zhifubao zhifu

---
+++ app/Http/Controllers/Alipay/PayController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Alipay;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PayController extends Controller
+{
+    //
+    public function notify(){
+        
+    }
+}
